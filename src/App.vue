97a3c1a6c04f1efc982ab64fa6eed7e991d94301
d-rivers <28294197+d-rivers@users.noreply.github.com>
2021-08-01 14:12:21 +0100

    included bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <Title title="Vue.js Playground"/>
+  <div class="container d-flex flex-column align-items-center">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Title title="Vue.js Playground"/>
+  </div>
 </template>
 
 <script src="./App.js"></script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue';
+
+import './bootstrap/bootstrap.scss';
+
 import App from './App.vue';
 
 createApp(App).mount('#app');
